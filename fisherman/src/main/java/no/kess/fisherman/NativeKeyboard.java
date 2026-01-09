095815b31287928f839c8045c8cca98e9944cde7
Åsmund Norderud <asmundno@live.no>
2026-01-09 13:54:25 +0100

    Refactor keyboard input handling: dedupe `sendKeyDown`/`sendKeyUp` logic into `sendInput`, clean up flags.

--- fisherman/src/main/java/no/kess/fisherman/NativeKeyboard.java
+++ fisherman/src/main/java/no/kess/fisherman/NativeKeyboard.java
@@ -10,32 +10,26 @@ public class NativeKeyboard {
     public static final int SCANCODE_F6 = 0x40;
     public static final int SCANCODE_F10 = 0x44;
 
+    private static final int KEYEVENTF_KEYUP = 0x0002;
+    private static final int KEYEVENTF_SCANCODE = 0x0008;
+
     public static void sendKeyDown(int scanCode) {
-        WinUser.INPUT input = new WinUser.INPUT();
-        input.type = new WinDef.DWORD(WinUser.INPUT.INPUT_KEYBOARD);
-        input.input.setType("ki");
-        input.input.ki.wScan = new WinDef.WORD(scanCode);
-        input.input.ki.time = new WinDef.DWORD(0);
-        input.input.ki.dwExtraInfo = new BaseTSD.ULONG_PTR(0);
-
-        // Flags: KEYEVENTF_SCANCODE (0x0008)
-        input.input.ki.wVk = new WinDef.WORD(0); // must be 0 for scancode
-        input.input.ki.dwFlags = new WinDef.DWORD(0x0008);
-
-        User32.INSTANCE.SendInput(new WinDef.DWORD(1), (WinUser.INPUT[]) input.toArray(1), input.size());
+        sendInput(scanCode, KEYEVENTF_SCANCODE);
     }
 
     public static void sendKeyUp(int scanCode) {
+        sendInput(scanCode, KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP);
+    }
+
+    private static void sendInput(int scanCode, int flags) {
         WinUser.INPUT input = new WinUser.INPUT();
         input.type = new WinDef.DWORD(WinUser.INPUT.INPUT_KEYBOARD);
         input.input.setType("ki");
         input.input.ki.wScan = new WinDef.WORD(scanCode);
         input.input.ki.time = new WinDef.DWORD(0);
         input.input.ki.dwExtraInfo = new BaseTSD.ULONG_PTR(0);
-
-        // Flags: KEYEVENTF_SCANCODE (0x0008) | KEYEVENTF_KEYUP (0x0002) = 0x000A
         input.input.ki.wVk = new WinDef.WORD(0);
-        input.input.ki.dwFlags = new WinDef.DWORD(0x0008 | 0x0002);
+        input.input.ki.dwFlags = new WinDef.DWORD(flags);
 
         User32.INSTANCE.SendInput(new WinDef.DWORD(1), (WinUser.INPUT[]) input.toArray(1), input.size());
     }
